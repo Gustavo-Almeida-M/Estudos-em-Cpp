4c1f4ec6c554f613c5b9ddaf85587c540517a13b
Gustavo Almeida <gustavoam916@gmail.com>
2023-03-05 19:06:38 -0300

    Começando a estudar C++

---
+++ teste.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+void saiHi(string name, int age)
+{
+    cout << "Hello " << name << " you have " << age << " years old" << endl;
+}
+int main()
+{
+    saiHi("Gustavo", 80);
+}
