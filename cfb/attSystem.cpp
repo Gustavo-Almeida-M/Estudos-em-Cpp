40ac78060235c9aab4419e69d3f7537fc263003b
@Gustavo-Almeida-M <gustavoam916@gmail.com>
2023-04-30 15:09:02 -0300

    Usando o system

---
+++ cfb/attSystem.cpp
@@ -0,0 +1,11 @@
+//Usando o comando System, que ser como os comandos do terminal
+#include <iostream>
+#include <stdlib.h>
+
+int main()
+{
+    std::system("color 1");
+    std::system("color/?");
+
+    return 0;
+}
