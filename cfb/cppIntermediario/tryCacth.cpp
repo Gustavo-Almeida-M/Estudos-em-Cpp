54c6fda5cfffb2678c1028ce41489b138efd3772
@Gustavo-Almeida-M <gustavoam916@gmail.com>
2023-05-27 15:50:50 -0300

    Usando try catch

---
+++ cfb/cppIntermediario/tryCacth.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <stdexcept>
+
+double divicao(double, double);
+
+int main()
+{
+    double valor1, valor2;
+
+    std::cin >> valor1 >> valor2;
+
+    try
+    {
+        std::cout << divicao(valor1, valor2) << '\n';
+    }
+    catch(const char* msg)
+    {
+        std::cout << "Erro: " << msg;
+    }
+    
+}
+
+double divicao(double v1, double v2)
+{
+    if(v2 == 0)
+    {
+        throw "divisao por 0!\n";
+
+    }
+    return v1/v2;
+}
